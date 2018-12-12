dcec0f316a5d5ece82528641f6161f5068507825
Vivien <vchang103@gmail.com>
2018-12-12 13:00:11 -0800

    get first part working

--- bamazon.sql
+++ bamazon.sql
@@ -3,3 +3,25 @@ DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price INT NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+("Books", "Books", 10, 100),
+("Echo", "Amazon", 55, 400),
+("Tacos", "Food", 3, 3000),
+("DVDs", "Movies", 4, 50),
+("VHS", "Movies", 1, 10),
+("Super Smash", "Games", 14, 1000),
+("Nintendo Switch", "Game Console", 100, 400),
+("PS4", "Game Console", 120, 300),
+("Xbox", "Game Console", 50, 500),
+("Zelda", "Games", 10, 5);
